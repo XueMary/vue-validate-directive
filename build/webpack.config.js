3ef49b89adb36555cd4751efbc0a01727bab9587
薛纪洪 <xuejihong@mockuai.com>
2021-10-09 15:31:08 +0800

    build(polyfill): es6 polyfill

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,36 +1,68 @@
-const path = require('path');
+const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-  mode: 'production',
-  entry: './library/index.ts',
+  mode: "production",
+  entry: "./library/index.ts",
   output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'vue-validate-directive.js',
+    path: path.resolve(__dirname, "../dist"),
+    filename: "vue-validate-directive.js",
     library: {
-      name: 'vueValidateDirective',
-      type: 'umd',
+      name: "vueValidateDirective",
+      type: "umd",
     },
-    clean: true
+    clean: true,
   },
   externals: {
-    "vue": "Vue"
+    vue: "Vue",
   },
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  useBuiltIns: "usage",
+                  corejs: {
+                    //core-js的版本
+                    version: 3,
+                  },
+                  //需要兼容的浏览器
+                  targets: {
+                    chrome: "23",
+                    firefox: "21",
+                    ie: "8",
+                    safari: "6",
+                    edge: "12",
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: path.resolve(__dirname, "../library/validate.css"), to: path.resolve(__dirname, "../dist") },
+        {
+          from: path.resolve(__dirname, "../library/validate.css"),
+          to: path.resolve(__dirname, "../dist"),
+        },
       ],
     }),
   ],
